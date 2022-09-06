005481e1c17028931883e0d7cc77725693845704
umit-yoda <103125938+umit-yoda@users.noreply.github.com>
2022-09-06 19:20:40 +0300

    Update altogic.js

--- examples/realtime-presence/src/altogic.js
+++ examples/realtime-presence/src/altogic.js
@@ -1,8 +1,8 @@
 import { createClient } from 'altogic';
 
 const altogic = createClient(
-	'https://012e-fo7m.c1-na.altogic.com',
-	'9e6c5808bb2c4a60b0a1de4f5f12ea19',
+	'<your Altogic app base url>',
+	'<your Altogic app client key>',
 	{
 		realtime: {
 			echoMessages: true,
