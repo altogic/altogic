533f76618d72c886491cc7dc0c3660db7b83e548
zinedkaloc <deniz@altogic.com>
2023-04-16 20:29:35 +0300

    add env variables

--- examples/stripe-saas-starter/src/helpers/altogic.js
+++ examples/stripe-saas-starter/src/helpers/altogic.js
@@ -1,13 +1,11 @@
 import { createClient } from "altogic";
 
-let envUrl = "https://c1-europe.altogic.com/e:6315a2d47fe76ffa635dc6f9";
-let clientKey = "882f5364e29140e794d0c031ea49f759";
+const ENV_URL = process.env.REACT_APP_ALTOGIC_ENV_URL;
+const CLIENT_KEY = process.env.REACT_APP_ALTOGIC_CLIENT_KEY;
+const API_KEY = process.env.REACT_APP_ALTOGIC_API_KEY;
 
-let options = {
-  apiKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnZJZCI6IjYzMTVhMmQ0N2ZlNzZmZmE2MzVkYzZmOSIsImtleUlkIjoiNjMxNWEyZDU3ZmU3NmZmYTYzNWRjNzBhIiwiaWF0IjoxNjYyMzYyMzI1LCJleHAiOjI1MjYzNjIzMjV9.c8Sr2eeZTD4riPUDwwp0qt_iXhRCyexbQRKhVLXUvXk",
-};
-
-const altogic = createClient(envUrl, clientKey, options);
+const altogic = createClient(ENV_URL, CLIENT_KEY, {
+  apiKey: API_KEY,
+});
 
 export { altogic };
